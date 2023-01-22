13588e71244aa42e27b098b6efd8bd98c24ace6f
Kevin Egan <kevinegan31@gmail.com>
2023-01-22 22:56:20 +0000

    Update code_signal_practice.py

--- Python/code_signal_practice.py
+++ Python/code_signal_practice.py
@@ -189,3 +189,33 @@ def solution(inputString):
 
 inputString = "foo(bar)baz(blim)"
 solution(inputString)
+
+
+# Alternating Sums
+# Several people are standing in a row and
+# need to be divided into two teams.
+# The first person goes into team 1, the second goes into team 2,
+# the third goes into team 1 again, the fourth into team 2, and so on.
+# You are given an array of positive integers -
+# the weights of the people.
+# Return an array of two integers,
+# where the first element is the total weight of team 1,
+# and the second element is the total weight of team 2 after the division is complete.
+def solution(a):
+    return [sum(a[::2]), sum(a[1::2])]
+
+
+a = [50, 60, 60, 45, 70]
+solution(a)
+
+# Add Border
+# Given a rectangular matrix of characters,
+# add a border of asterisks(*) to it.
+def solution(picture):
+    ast = "*" * (len(picture[0]) + 2)
+    picture = [ast] + [f"*{i}*" for i in picture] + [ast]
+    return picture
+
+
+picture = ["abc", "ded"]
+solution(picture)
