b73b7064bb8b8170ab4375d5d24a512b97e15f59
Kevin Egan <kevinegan31@gmail.com>
2023-02-28 10:45:50 +0000

    Update 101_pandas_exercises.py

--- Data_Manipulation_with_Python/101_pandas_exercises.py
+++ Data_Manipulation_with_Python/101_pandas_exercises.py
@@ -56,6 +56,7 @@ ser2 = pd.Series([4, 5, 6, 7, 8])
 
 pd.concat([ser1[~np.isin(ser1, ser2)], ser2[~np.isin(ser2, ser1)]])
 
+
 # 8. Compute the minimum, 25th percentile,
 # median, 75th, and maximum of ser.
 np.random.RandomState(100)
@@ -215,7 +216,7 @@ peak_locs
 my_str = 'dbc deb abed gade'
 from collections import Counter
 min(Counter(my_str).values())
-my_str.replace(' ', Counter(my_str).min())
+my_str.replace(' ', min(Counter(my_str)))
 
 
 def fizzbuzz(n):
@@ -264,3 +265,293 @@ def Fizzbuzz(n):
             print(n_int)
             
 Fizzbuzz(15)
+
+import pandas as pd
+
+transactions = {"transaction_id" : [1, 2, 3, 4, 5], "product_id" : [101, 102, 103, 104, 105], "amount" : [3, 5, 8, 3, 2]}
+
+products = {"product_id" : [101, 102, 103, 104, 105], "price" : [20.00, 21.00, 15.00, 16.00, 52.00]}
+
+df_transactions = pd.DataFrame(transactions)
+
+df_products = pd.DataFrame(products)
+
+df = pd.merge(df_transactions, df_products, on='product_id')
+df['total_value'] = df['amount'] * df['price']
+df.loc[df['total_value']> 100]
+
+# Write a function complete_address
+# to create a single dataframe with complete addresses
+# in the format of street, city, state, zip code.
+import pandas as pd
+
+addresses = {"address": ["4860 Sunset Boulevard, San Francisco, 94105", "3055 Paradise Lane, Salt Lake City, 84103", "682 Main Street, Detroit, 48204", "9001 Cascade Road, Kansas City, 64102", "5853 Leon Street, Tampa, 33605"]}
+
+cities = {"city": ["Salt Lake City", "Kansas City", "Detroit", "Tampa", "San Francisco"],
+          "state": ["Utah", "Missouri", "Michigan", "Florida", "California"]}
+
+df_addresses = pd.DataFrame(addresses)
+df_cities = pd.DataFrame(cities)
+df_cities
+df_addresses[['street', 'city', 'zipcode']] = df_addresses["address"].apply(lambda x: pd.Series(str(x).split(", ")))
+df_addresses = df_addresses.merge(df_cities, on='city')
+df_addresses['address'] = df_addresses[['street', 'city', 'state', 'zipcode']].agg(', '.join, axis=1)
+df_addresses.drop(columns=['street', 'city', 'state', 'zipcode'])
+
+# You’re given a dataframe df_rain containing rainfall data.
+# The dataframe has two columns: day of the week and rainfall in inches.
+
+# Write a function median_rainfall to find the
+# median amount of rainfall for the days on which it rained. 
+import pandas as pd
+
+rainfall = {"Day" : ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+            "Inches" : [0, 1.2, 0, 0.8, 1]}
+
+df_rain = pd.DataFrame(rainfall)
+df_rain.select_dtypes(include='Inches') != 0
+
+df_rain[df_rain['Inches'] !=0].median(numeric_only=True)[0]
+
+# Write a function named grades_colors to select only the rows
+# where the student’s favorite color is green or red and their grade is above 90.
+import pandas as pd
+
+students = {"name" : ["Tim Voss", "Nicole Johnson", "Elsa Williams", "John James", "Catherine Jones"],
+            "age" : [19, 20, 21, 20, 23],
+            "favorite_color" : ["red", "yellow", "green", "blue", "green"],
+            "grade" : [91, 95, 82, 75, 93]}
+
+students_df = pd.DataFrame(students)
+students_df[((students_df['favorite_color'] == 'red') | (students_df['favorite_color'] == 'green')) & (students_df['grade'] >90)]
+
+
+df = pd.Series([-34, 40, -89, 5, -26])
+mu0 = 1
+df.mean()
+df.sem()
+(df.mean() - mu0) / df.sem()
+
+
+def fizzbuzz(n):
+    for FizzBuzz in range(n+1):
+        if FizzBuzz % 5 == 0 and FizzBuzz % 3 == 0:
+            print("FizzBuzz")
+            continue
+        elif FizzBuzz % 3 == 0:
+            print("Fizz")
+            continue
+        elif FizzBuzz % 5 == 0:
+            print("Buzz")
+            continue
+        else:
+            print(FizzBuzz)
+fizzbuzz(15)
+
+
+# 29. How to replace missing spaces in a string with the least frequent character?
+my_str = 'dbc deb abed gade'
+ser = pd.Series(list(my_str))
+freq = ser.value_counts()
+freq
+least_freq = freq.dropna().index[-1]
+least_freq
+''.join(ser.replace(' ', least_freq))
+
+# 30. How to create a TimeSeries starting ‘2000-01-01’ and
+# 10 weekends (saturdays) after that having random numbers as values?
+
+
+
+
+# Write a Pandas program to select the specified columns and rows from a given DataFrame.
+# Select 'name' and 'score' columns in rows 1, 3, 5, 6 from the following data frame.
+import pandas as pd
+import numpy as np
+
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data, index=labels)
+df.iloc[[1,3,5,6],[0,1]]
+df[['name', 'score']].iloc[[1,3,5,6]]
+
+# Write a Pandas program to select the rows
+# where the number of attempts in the examination is greater than 2.
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts' : [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data, index=labels)
+df[df['attempts'] > 2]
+
+# Write a Pandas program to count the number of rows and columns of a DataFrame.
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data, index=labels)
+df.shape[0] # rows
+df.shape[1] # columns
+
+# Write a Pandas program to select the rows where the score is missing
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data, index=labels)
+df[df['score'].isnull()]
+
+# Write a Pandas program to select the rows
+# the score is between 15 and 20 (inclusive).
+
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data, index=labels)
+df[(df['score'] >= 15) & (df['score'] <= 20)]
+df[df['score'].between(15,20)]
+
+# Write a Pandas program to select the rows
+# where number of attempts in the examination
+# is less than 2 and score greater than 15.
+
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data, index=labels)
+df[(df['score'] > 15) & (df['attempts'] < 2)]
+
+# Write a Pandas program to change the score in row 'd' to 11.5.
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data, index=labels)
+df_new = df[['name', 'score', 'attempts']]
+df_new
+
+df
+df.loc['d','score'] = 11.5
+df
+
+# Write a Pandas program to calculate the
+# sum of the examination attempts by the students
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data, index=labels)
+df['attempts'].sum()
+
+# Write a Pandas program to calculate the
+# mean score for each different student in DataFrame.
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data, index=labels)
+df.score.mean()
+
+# Write a Pandas program to append a new row 'k'
+# to data frame with given values for each column.
+# Now delete the new row and return the original DataFrame.
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data, index=labels)
+new_data = ['Kevin', 19, 1, 'yes']
+df.loc['k'] = new_data
+df
+df.drop('k')
+
+# Write a Pandas program to sort the DataFrame
+# first by 'name' in descending order,
+# then by 'score' in ascending order. 
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data, index=labels)
+df.sort_values(by=['name', 'score'], ascending=[False, True])
+
+# Write a Pandas program to replace the 'qualify' column
+# contains the values 'yes' and 'no' with True and False.
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data, index=labels)
+df.replace({'qualify':{'yes':True, 'no':False}})
+
+# Write a Pandas program to change the name
+# 'James' to 'Suresh' in name column of the DataFrame.
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data, index=labels)
+df['name'] = df['name'].replace('James', 'Suresh')
+df
+
+# Write a Pandas program to delete the 'attempts' column
+# from the DataFrame.
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data, index=labels)
+df.drop(columns='attempts')
+
+# Write a Pandas program to insert a
+# new column in existing DataFrame.
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df = pd.DataFrame(exam_data , index=labels)
+color = ['Red','Blue','Orange','Red','White','White','Blue','Green','Green','Red']
+df['color'] = color
+df
+
+# Write a Pandas program to iterate over rows in a DataFrame.
+exam_data = [{'name':'Anastasia', 'score':12.5}, {'name':'Dima','score':9}, {'name':'Katherine','score':16.5}]
+df = pd.DataFrame(exam_data)
+df
+
+for index, rows in df.iterrows():
+    print(rows['name'], rows['score'])
+
